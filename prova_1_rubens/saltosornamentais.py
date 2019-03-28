ad8cef10c429e3c1de41235753571c90b921ce6d
Iago Sousa <fciagsousa94@gmail.com>
2019-03-28 14:28:09 -0300

    corrigindo questao da prova do rubens

--- prova_1_rubens/saltosornamentais.py
+++ prova_1_rubens/saltosornamentais.py
@@ -1,24 +1,18 @@
-maxima = float(input())
-minima = float(input())
+nota1 = float(input())
 
-if maxima < minima:
-    aux = maxima
-    maxima = minima
-    minima = maxima
-    #maxima, minima = minima, maxima
-    
-soma = maxima + minima
+max = nota1
+min = nota1
+soma = nota1
 
-for i in range(3):
+for i in range(4):
     nota = float(input())
-    if nota > maxima:
-        maxima = nota
-    if nota < minima:
-        minima = nota
+
+    if nota > max:
+        max = nota
+    if nota < min:
+        min = nota
     soma += nota
 
-soma -= (minima+maxima)
+media = (soma - min - max) / 3
 
-media = soma / 3
-
-print("%.2f" % media)
+prfloat("%.2f" % media)
