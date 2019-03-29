be3eb3d99b05c1e3114ce16c257a7fa3bbd64cd1
Iago Sousa <fciagsousa94@gmail.com>
2019-03-29 09:04:57 -0300

    removendo codigos de DD

--- DD_27_03/arredondamento.py
+++
@@ -1,8 +0,0 @@
-nota = float(input())
-nota_quebrada = nota - int(nota)
-nota_inteira = nota - nota_quebrada
-
-if nota_quebrada > 0.5:
-    print("Nota: ", (nota_inteira+1))
-else:
-    print("Nota: ", nota_inteira)
